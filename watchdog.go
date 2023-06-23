8304eea8438314045d9354592c2831a1df5c8f48
neekonsu <neekon@berkeley.edu>
2023-06-23 11:33:13 -0700

    Included doc comments for command and methods.

--- watchdog.go
+++ watchdog.go
@@ -1,4 +1,32 @@
+/*
+[watchdog command manual]
+watchdog notifies silent stoppages of the Power Automate + DSView laboratory setup used to record digital PSE frameware logs for debugging.
+The watchdog expects the path used to log DSView (digital logic analyzer) output (.dsl files). Crashes will be pushed to the #power-automate-slackbot channel in Presidio Medical Slack.
+
+Usage:
+watchdog [--path PATH]
+
+Options:
+
+	--path	Absolute or relative path to DSView output directory.
+			Default: "$HOME"
+*/
 package main
+
+// fileTimeout : timeout if new data is not logged in target directory after 1.5 logging cycles (15min)
+func fileTimeout() {
+
+}
+
+// dsvOpen : poll status of DSView and throw err if DSView is closed
+func dsvOpen() {
+
+}
+
+// paOpen : poll status of Power Automate and throw err if Power Automate is closed
+func paOpen() {
+
+}
 
 func main() {
 
