a14e2b732b7fce82cf88cdae005b378fc7a5c0b4
neekonsu <neekon@berkeley.edu>
2023-06-23 19:14:39 -0700

    First draft of freshFiles, storage TODO: dsview, powerautomate, and code review

--- watchdog.go
+++ watchdog.go
@@ -15,10 +15,13 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
+	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
@@ -35,10 +38,12 @@ func poller(target string, stopCh chan struct{}) {
 	ticker := time.NewTicker(10 * time.Minute)
 	defer ticker.Stop()
 	for range ticker.C {
-		freshFiles()
-		storage()
-		dsview()
-		powerautomate()
+		// non-blocking deployment of checks, concurrent interrup of all goroutines if stoppage found
+		// TODO: verify deployment of goroutines here yields deterministic behavior
+		go freshFiles(target, stopCh)
+		go storage(stopCh)
+		go dsview(stopCh)
+		go powerautomate(stopCh)
 		select {
 		case <-stopCh:
 			return
@@ -47,38 +52,100 @@ func poller(target string, stopCh chan struct{}) {
 	}
 }
 
-// freshFiles : timeout if new data is not logged in target directory after 1.5 logging cycles (15min) by checking time since most recent file creation in path
-func freshFiles() {
+// age : return the (integer) number of minutes since the modification of the newest file in TARGET
+func age(path string) (int, error) {
+	var recentModTime time.Time
 
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return 0, err
+	}
+
+	for _, file := range files {
+		if file.Mode().IsRegular() {
+			modTime := file.ModTime()
+			if modTime.After(recentModTime) {
+				recentModTime = modTime
+			}
+		}
+	}
+
+	if recentModTime.IsZero() {
+		return 0, fmt.Errorf("no files found in the directory")
+	}
+
+	minutesAgo := int(time.Since(recentModTime).Round(time.Minute).Minutes())
+	return minutesAgo, nil
+}
+
+// freshFiles : timeout if new data is not logged in target directory after 1.5 logging cycles (15min) by checking time since most recent file creation in path
+func freshFiles(target string, stopCh chan struct{}) {
+	if fileAge, err := age(target); err != nil || fileAge > 10 {
+		defer close(stopCh)
+		return
+	}
+}
+
+// getFreeSpace : return the (uint64) number of free bytes on the windows system
+func getFreeSpace(path string) (uint64, error) {
+	fs := syscall.MustLoadDLL("kernel32.dll")
+	getDiskFreeSpaceEx := fs.MustFindProc("GetDiskFreeSpaceExW")
+
+	freeBytesAvailable := uint64(0)
+	totalBytes := uint64(0)
+	totalFreeBytes := uint64(0)
+
+	_, _, err := getDiskFreeSpaceEx.Call(
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(path))),
+		uintptr(unsafe.Pointer(&freeBytesAvailable)),
+		uintptr(unsafe.Pointer(&totalBytes)),
+		uintptr(unsafe.Pointer(&totalFreeBytes)),
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return freeBytesAvailable, nil
 }
 
 // storage : check for remaining storage and throw err if less than 10G is free
-func storage() {
-
+func storage(stopCh chan struct{}) {
+	tenG := uint64(1000000000)
+	if space, err := getFreeSpace("C:\\"); err != nil || space < tenG {
+		defer close(stopCh)
+		return
+	}
 }
 
 // dsview : check status of DSView and throw err if DSView is closed
-func dsview() {
-
+func dsview(stopCh chan struct{}) {
+	// TODO
 }
 
 // powerautomate : check status of Power Automate and throw err if Power Automate is closed
-func powerautomate() {
+func powerautomate(stopCh chan struct{}) {
+	// TODO
+}
 
+// notify : awaits forseen stoppage to notify Slack
+func notify(stopCh chan struct{}) {
+	<-stopCh
+	fmt.Println("Encountered a silent stoppage!")
 }
 
 // main : indefinitely awaits the stop signal or external termination and notifies Slack before exiting.
 func main() {
 	// Decide the directory to watch
 	path := os.Args[1]
-	TARGET, err := os.UserHomeDir();
+	TARGET, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal("Failed to get user home directory")
 	}
 	if path != "" {
 		TARGET = os.Args[1]
 	}
-	
+
 	// Get current process handle (Windows Process) to address parameters
 	handle := windows.CurrentProcess()
 
@@ -90,6 +157,7 @@ func main() {
 	// Create a channel for intentional interrupts and deploy poller on this signal
 	stopCh := make(chan struct{})
 	go poller(TARGET, stopCh)
+	go notify(stopCh)
 
 	// Create a channel to receive os interrupt signals
 	osSignals := make(chan os.Signal, 1)
